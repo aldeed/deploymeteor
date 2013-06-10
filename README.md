d65a210a0902eefb579331a4403276ea5d3ab60c
Eric Dobbertin <eric@dairystatedesigns.com>
2013-06-10 10:08:19 -0500

    made better

--- README.md
+++ README.md
@@ -4,33 +4,49 @@ The deploymeteor script makes it as easy as possible to deploy a meteor app to a
 
 ## Install deploymeteor
 
-Just run command in your terminal on Mac/Linux:
+To install deploymeteor on your workstation (Mac or Linux), run this command in your terminal:
 
 ```bash
 $ sudo -H curl https://raw.github.com/aldeed/deploymeteor/master/install | sh
 ```
 
-## Prerequisites
+## Setting Up the Server
 
-1. Launch a new EC2 server and make note of its hostname. You can deploy multiple meteor apps/websites to this one server.
-2. Before deploying each app, make sure that the app's directory is under git version control:
+1. Launch a new EC2 server and make note of its hostname. You can deploy multiple meteor apps/websites to this one server. Make sure to enable SSH (port 22) and HTTP (port 80). You might want to open additional ports or port ranges for connecting directly to your meteor apps.
+2. SSH into the EC2 server and enter `sudo visudo`. Near the bottom, press I to switch to insert mode and insert a ! before `requiretty`. This is necessary for the script to work correctly. Press ESC and enter `:w!`. Now enter `:q` to quit.
+3. On your workstation, run `deploymeteor prepserver`. Answer the prompts. The host is the one you noted in step 1 and the key file is the one you downloaded while setting up the EC2 server.
 
-```bash
-$ cd /my/app/directory
-$ git init
-```
+You may now use this server to host one or more meteor apps.
 
 ## Deploy an App the First Time
 
-Assuming the app directory is under git version control and you've committed all your changes, just run one command and answer the prompts:
+Let's assume that you created a meteor app locally, and now you're ready to deploy it to your server.
 
 ```bash
 $ cd /my/app/directory
 $ deploymeteor <env>
 ```
 
-Replace <env> with whatever you want to call the environment. This environment will be created and initialized on the remote server and added as a remote repository for this git repository.
+Replace <env> with whatever you want to call the environment, and answer all the prompts. This environment will be created and initialized on the remote server and added as a remote repository for this git repository.
+
+If the current directory isn't already under git version control when you run deploymeteor, the script initializes the git repo for you, adds all files, and does an initial commit.
+
+## Subsequent Updates
+
+You only need to run deploymeteor once per project environment. You should run it again if any of the information in the prompts changes, for example, if your database or e-mail URLs need to change.
+
+After that first run, simply push to the correct remote env to deploy the current branch:
+
+```bash
+$ git push <env>
+```
+
+Where <env> is the same thing you entered when initially running deploymeteor. The initial `git push` is done for you during the deploymeteor script, so you don't need to do it again until you've made and committed more changes.
+
+## How it Works
+
+deploymeteor sets up a git post-receive hook script on the remote repository. Every time you push to that environment, the script runs, causing the files to be updated and the app restarted, using forever to keep it running, well, forever.
 
 ## Thanks
 
-Thanks to the creators of meteoric.sh and http://toroid.org/ams/git-website-howto.
+Thanks to @julien-c for [https://github.com/julien-c/meteoric.sh meteoric.sh] and credit to [http://toroid.org/ams/git-website-howto this post].
--- deploymeteor.sh
+++ deploymeteor.sh
@@ -1,85 +1,168 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
+	echo
 	echo "Usage: deploymeteor <environment>"
 	echo "For example, type deploymeteor staging to deploy to a staging environment. This environment will be created and initialized on the remote server and added as a remote repository for this git repository."
+	echo
+	echo "Before the first time you use deploymeteor with a new server, you should also run deploymeteor prepserver. This will install node, npm, meteor, forever, and meteorite on the server."
 	exit 1
 fi
 
-##make sure git init has been run
-if [ ! -d ".git" ]; then
-	git init
-	git add .
-	git commit -a -m "Initial commit"
-fi
+HOME_DIR=/home/ec2-user
 
-APP_NAME=${PWD##*/}
-
-#prompt for needed info
-read -e -p "Enter the hostname or IP address of the Amazon Linux EC2 server (e.g., 11.111.11.111 or ec2-11-111-11-111.us-west-2.compute.amazonaws.com): " APP_HOST
-read -e -p "Enter the path to your EC2 .pem file on this machine: " EC2_PEM_FILE
-echo "Enter the root URL for this website, as users will access it, including protocol."
-echo "Examples: http://mysite.com or https://mysite.com"
-echo "Default (press ENTER): http://$APP_HOST"
-read -e -p "Root URL: " ROOT_URL
-ROOT_URL=${ROOT_URL:-http://$APP_HOST}
-echo "Enter the port on which to host this website."
-echo "Examples: 8080 or 3001"
-echo "Default (press ENTER): 80"
-read -e -p "Port: " PORT
-PORT=${PORT:-80}
-echo "Enter the hostname or IP address of the server hosting your MongoDB database."
-echo "Default (press ENTER): localhost"
-read -e -p "MongoDB host: " MONGO_HOST
-MONGO_HOST=${MONGO_HOST:-localhost}
-echo "Enter the port through which your MongoDB database is accessible."
-echo "Default (press ENTER): 27017"
-read -e -p "MongoDB port: " MONGO_PORT 
-MONGO_PORT=${MONGO_PORT:-27017}
-echo "Enter the name of your MongoDB database."
-echo "Default (press ENTER): $APP_NAME"
-read -e -p "MongoDB database: " MONGO_DBNAME
-MONGO_DBNAME=${MONGO_DBNAME:$APP_NAME}
-APP_DIR=/home/meteor
-LOG_DIR=$APP_DIR/logs/$APP_NAME/
-BUNDLE_DIR=$APP_DIR/bundles/$APP_NAME/
-ROOT_URL=http://$APP_HOST
-MONGO_URL=mongodb://$MONGO_HOST:$MONGO_PORT/MONGO_DBNAME
-SSH_HOST="ec2-user@$APP_HOST" SSH_OPT="-i $EC2_PEM_FILE"
-
-SETUP="
+PREP="
 sudo yum install gcc-c++ make;
 sudo yum install openssl-devel;
 sudo yum install git;
+cd $HOME_DIR;
 git clone git://github.com/joyent/node.git;
 cd node;
 git checkout v0.10.10;
 ./configure;
 make;
 sudo make install;
+cd ..;
 sudo -H npm install -g forever;
 curl https://install.meteor.com | /bin/sh;
 sudo -H npm install -g meteorite;
-mkdir -p $APP_DIR;
-cd $APP_DIR;
-mkdir -p bundles
-mkdir -p logs
-mkdir -p $APP_NAME.git;
-cd $APP_NAME.git;
-git init;
-cat > hooks/post-receive <<ENDCAT
-#!/bin/sh
-mrt bundle ../bundles/$APP_NAME/bundle.tgz;
-cd ../bundles/$APP_NAME/
-tar -zxvf bundle.tgz;
-export MONGO_URL=$MONGO_URL;
-export ROOT_URL=$ROOT_URL;
-export PORT=$PORT;
-sudo forever start -l $LOG_DIR/forever.log -o $LOG_DIR/out.log -e $LOG_DIR/err.log bundle/main.js;
-ENDCAT;
-chmod +x hooks/post-receive;
 "
 
-ssh $SSH_OPT $SSH_HOST $SETUP;
-git remote add $1 ssh://$SSH_HOST$APP_DIR/$APP_NAME.git;
-git push $1 +master:refs/heads/master;
+#Eventually should make prepserver upgrade them if they're already installed
+#This is how you upgrade node:
+#sudo npm cache clean -f
+#sudo npm install -g n
+#sudo n stable
+
+#If this has been run before, grab variable defaults from stored config file
+PWD=`pwd`
+if [ -r "$PWD/.deploymeteor.config" ]; then
+	source "$PWD/.deploymeteor.config"
+fi
+
+#prompt for info needed by both prepserver and environment deploy
+echo
+echo "Enter the hostname or IP address of the Amazon Linux EC2 server."
+echo "Examples: 11.111.11.111 or ec2-11-111-11-111.us-west-2.compute.amazonaws.com"
+if [ -z "$LAST_APP_HOST" ]; then
+	echo "Default (press ENTER): No default"
+else
+	echo "Default (press ENTER): $LAST_APP_HOST"
+fi
+read -e -p "Host: " APP_HOST
+APP_HOST=${APP_HOST:-$LAST_APP_HOST}
+echo
+echo "Enter the path to your EC2 .pem file on this machine."
+if [ -z "$LAST_EC2_PEM_FILE" ]; then
+	echo "Default (press ENTER): No default"
+else
+	echo "Default (press ENTER): $LAST_EC2_PEM_FILE"
+fi
+read -e -p "Key file: " EC2_PEM_FILE
+EC2_PEM_FILE=${EC2_PEM_FILE:-$LAST_EC2_PEM_FILE}
+
+cat > $PWD/.deploymeteor.config <<ENDCAT
+LAST_APP_HOST=$APP_HOST
+LAST_EC2_PEM_FILE=$EC2_PEM_FILE
+ENDCAT
+
+SSH_HOST="ec2-user@$APP_HOST"
+SSH_OPT="-i $EC2_PEM_FILE"
+
+case "$1" in
+prepserver)
+	echo
+	echo "Preparing server…"
+	ssh -t $SSH_OPT $SSH_HOST $PREP
+	echo "Done!"
+	exit 1
+	;;
+esac
+
+####The rest is run only for setting up git deployment####
+APPS_DIR=$HOME_DIR/meteorapps
+APP_NAME=${PWD##*/}
+
+#Prompt for additional app-specific info that is needed
+echo
+echo "Enter the root URL for this website, as users will access it, including protocol."
+echo "Examples: http://mysite.com or https://mysite.com"
+echo "Default (press ENTER): http://$APP_HOST"
+echo
+read -e -p "Root URL: " ROOT_URL
+ROOT_URL=${ROOT_URL:-http://$APP_HOST}
+echo
+echo "Enter the port on which to host this website."
+echo "Examples: 8080 or 3001"
+echo "Default (press ENTER): 80"
+echo
+read -e -p "Port: " PORT
+PORT=${PORT:-80}
+echo
+echo "Enter the URL for the MongoDB database used by this app."
+echo "Format: mongodb://<username>:<password>@<ip or hostname>:<port>/<dbname>"
+read -e -p "MongoDB URL: " MONGO_URL
+echo
+echo
+echo "Enter the SMTP URL for e-mail sending by this app."
+echo "Format: smtp://<username>:<password>@<ip or hostname>:<port>"
+read -e -p "E-mail URL: " MAIL_URL
+echo
+
+GIT_APP_DIR=$APPS_DIR/$APP_NAME/git
+WWW_APP_DIR=$APPS_DIR/$APP_NAME/www
+LOG_DIR=$APPS_DIR/$APP_NAME/logs
+BUNDLE_DIR=$APPS_DIR/$APP_NAME/bundle
+ROOT_URL=http://$APP_HOST
+
+ENVSETUP="
+mkdir -p $APPS_DIR/$APP_NAME;
+rm -rf $GIT_APP_DIR;
+mkdir -p $GIT_APP_DIR/hooks;
+mkdir -p $WWW_APP_DIR;
+mkdir -p $LOG_DIR;
+mkdir -p $BUNDLE_DIR;
+cd $GIT_APP_DIR;
+git init --bare;
+touch hooks/post-receive;
+sudo chmod +x hooks/post-receive;
+"
+
+##on workstation, make sure git init has been run
+if [ ! -d ".git" ]; then
+	git init
+	git add .
+	git commit -a -m "Initial commit"
+fi
+
+echo
+echo
+echo "Setting up git deployment for the $1 environment of $APP_NAME"
+ssh -t $SSH_OPT $SSH_HOST $ENVSETUP
+cat > tmp-post-receive <<ENDCAT
+#!/bin/sh
+GIT_WORK_TREE=$WWW_APP_DIR git checkout -f
+cd $WWW_APP_DIR
+mrt bundle $APPS_DIR/$APP_NAME/bundle.tgz
+cd $APPS_DIR/$APP_NAME
+tar -zxvf bundle.tgz
+rm bundle.tgz
+#rebuild fibers
+cd $BUNDLE_DIR/server/node_modules
+rm -r fibers
+npm install fibers@1.0.0
+#set up variables
+cd $BUNDLE_DIR
+sudo PORT=$PORT ROOT_URL="$ROOT_URL" MONGO_URL="$MONGO_URL" MAIL_URL="$MAIL_URL" forever start -l $LOG_DIR/forever.log -o $LOG_DIR/out.log -e $LOG_DIR/err.log -a main.js
+ENDCAT
+scp $SSH_OPT tmp-post-receive $SSH_HOST:$GIT_APP_DIR/hooks/post-receive
+rm tmp-post-receive
+ssh-add $EC2_PEM_FILE
+git remote rm $1
+git remote add $1 ssh://$SSH_HOST$GIT_APP_DIR
+git push $1 master
+echo
+echo
+echo
+echo "Done! Now simply make and commit your changes and then redeploy as necessary with git push $1"
+echo
